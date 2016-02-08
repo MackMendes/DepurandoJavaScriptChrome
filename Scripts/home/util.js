358ef306c0eba0eeea2e69190f7800c183b70d91
Charles Mendes de Macedo <charlesmendes_31@hotmail.com>
2016-02-08 20:19:44 -0200

    Exemplo Artigo
    Exemplo Artigo

--- Scripts/home/util.js
+++ Scripts/home/util.js
@@ -3,6 +3,8 @@
 // jQuery = $
 // Função ready do Jquery, executa a função que recebe, após o navegador carregar (renderizar) toda a página
 jQuery(document).ready(function () {
+    // debugger: uma palavra reservada do JavaScript para debugar o código. Quando o Navegador estiver com o Console (f12) dele aberto, vai parar a interpretação
+    // do código quando encontrar esse palavra
     debugger;
     ExecutarTodasFuncoes();
 });
@@ -10,7 +12,7 @@ jQuery(document).ready(function () {
 // $(function() {});
 
 // Função Nomeada (SetYearFooter)
-function SetYearFooter() {
+function AtribuirAnoFooter() {
     // Quando for pegar um elemento HTML por ID, utiliza-se '#' na frente e o ID do elemento
     // Por boa prática, utiliza-se '$' na frente da variável que for armazenar um objeto Jquery
     var $lblFooter = $('#lbltimeNow');
@@ -18,10 +20,14 @@ function SetYearFooter() {
     $lblFooter.text(new Date().getFullYear().toString());
 }
 
+function AtribuirTituloPagina() {
+    $('.titlePage').text('Clientes');
+}
 
 function ExecutarTodasFuncoes() {
-    SetYearFooter();
+    AtribuirAnoFooter();
     AtribuiOnBlurCep();
+    AtribuirTituloPagina();
 }
 
 
